d68686aecc2f69e6e5cd9de24965d6f35775073f
Abdülsamet Akan <akansametgs@gmail.com>
2026-01-27 14:44:25 +0300

    sitemapweekly

--- kurabiye-log/app/sitemap.ts
+++ kurabiye-log/app/sitemap.ts
@@ -19,7 +19,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     const postRoutes = posts.map((post) => ({
         url: `${baseUrl}/blog/${post.slug}`,
         lastModified: new Date(post.date),
-        changeFrequency: 'monthly' as const,
+        changeFrequency: 'weekly' as const,
         priority: 0.8,
     }));
 
